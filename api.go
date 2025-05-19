3b9ccbff5c09d946b0edb4f4d356f3664fa1703c
Corey McFadden <cmcfadden@clearlyip.com>
2025-05-18 23:55:13 -0400

    Initial commit

---
+++ api.go
@@ -0,0 +1,83 @@
+package elevenlabs_websockets
+
+type UserData struct {
+	UserID                         string             `json:"user_id"`
+	Subscription                   Subscription       `json:"subscription"`
+	IsNewUser                      bool               `json:"is_new_user"`
+	CanUseDelayedPaymentMethods    bool               `json:"can_use_delayed_payment_methods"`
+	IsOnboardingCompleted          bool               `json:"is_onboarding_completed"`
+	IsOnboardingChecklistCompleted bool               `json:"is_onboarding_checklist_completed"`
+	SubscriptionExtras             SubscriptionExtras `json:"subscription_extras"`
+	XIApiKey                       string             `json:"xi_api_key"`
+	FirstName                      string             `json:"first_name"`
+	IsAPIKeyHashed                 bool               `json:"is_api_key_hashed"`
+	XIApiKeyPreview                string             `json:"xi_api_key_preview"`
+	ReferralLinkCode               string             `json:"referral_link_code"`
+	PartnerstackPartnerDefaultLink string             `json:"partnerstack_partner_default_link"`
+}
+
+type Subscription struct {
+	Tier                           string `json:"tier"`
+	CharacterCount                 int    `json:"character_count"`
+	CharacterLimit                 int    `json:"character_limit"`
+	CanExtendCharacterLimit        bool   `json:"can_extend_character_limit"`
+	AllowedToExtendCharacterLimit  bool   `json:"allowed_to_extend_character_limit"`
+	VoiceSlotsUsed                 int    `json:"voice_slots_used"`
+	ProfessionalVoiceSlotsUsed     int    `json:"professional_voice_slots_used"`
+	VoiceLimit                     int    `json:"voice_limit"`
+	VoiceAddEditCounter            int    `json:"voice_add_edit_counter"`
+	ProfessionalVoiceLimit         int    `json:"professional_voice_limit"`
+	CanExtendVoiceLimit            bool   `json:"can_extend_voice_limit"`
+	CanUseInstantVoiceCloning      bool   `json:"can_use_instant_voice_cloning"`
+	CanUseProfessionalVoiceCloning bool   `json:"can_use_professional_voice_cloning"`
+	Status                         string `json:"status"`
+	MaxCharacterLimitExtension     int    `json:"max_character_limit_extension"`
+	NextCharacterCountResetUnix    int64  `json:"next_character_count_reset_unix"`
+	MaxVoiceAddEdits               int    `json:"max_voice_add_edits"`
+	Currency                       string `json:"currency"`
+	BillingPeriod                  string `json:"billing_period"`
+	CharacterRefreshPeriod         string `json:"character_refresh_period"`
+}
+
+type SubscriptionExtras struct {
+	Concurrency                                    int        `json:"concurrency"`
+	ConvaiConcurrency                              int        `json:"convai_concurrency"`
+	ForceLoggingDisabled                           bool       `json:"force_logging_disabled"`
+	CanRequestManualProVoiceVerification           bool       `json:"can_request_manual_pro_voice_verification"`
+	CanBypassVoiceCaptcha                          bool       `json:"can_bypass_voice_captcha"`
+	Moderation                                     Moderation `json:"moderation"`
+	ConvaiCharsPerMinute                           int        `json:"convai_chars_per_minute"`
+	ConvaiASRCharsPerMinute                        int        `json:"convai_asr_chars_per_minute"`
+	UnusedCharactersRolledOverFromPreviousPeriod   int        `json:"unused_characters_rolled_over_from_previous_period"`
+	OverusedCharactersRolledOverFromPreviousPeriod int        `json:"overused_characters_rolled_over_from_previous_period"`
+	Usage                                          Usage      `json:"usage"`
+}
+
+type Moderation struct {
+	IsInProbation                         bool   `json:"is_in_probation"`
+	EnterpriseCheckNogoVoice              bool   `json:"enterprise_check_nogo_voice"`
+	EnterpriseCheckBlockNogoVoice         bool   `json:"enterprise_check_block_nogo_voice"`
+	NeverLiveModerate                     bool   `json:"never_live_moderate"`
+	NogoVoiceSimilarVoiceUploadCount      int    `json:"nogo_voice_similar_voice_upload_count"`
+	EnterpriseBackgroundModerationEnabled bool   `json:"enterprise_background_moderation_enabled"`
+	OnWatchlist                           bool   `json:"on_watchlist"`
+	SafetyStatus                          string `json:"safety_status"`
+	WarningStatus                         string `json:"warning_status"`
+}
+
+type Usage struct {
+	RolloverCreditsQuota          int `json:"rollover_credits_quota"`
+	SubscriptionCycleCreditsQuota int `json:"subscription_cycle_credits_quota"`
+	ManuallyGiftedCreditsQuota    int `json:"manually_gifted_credits_quota"`
+	RolloverCreditsUsed           int `json:"rollover_credits_used"`
+	SubscriptionCycleCreditsUsed  int `json:"subscription_cycle_credits_used"`
+	ManuallyGiftedCreditsUsed     int `json:"manually_gifted_credits_used"`
+	PaidUsageBasedCreditsUsed     int `json:"paid_usage_based_credits_used"`
+	ActualReportedCredits         int `json:"actual_reported_credits"`
+}
+
+type StreamingAlignmentSegment struct {
+	CharStartTimesMs []int    `json:"charStartTimesMs"`
+	CharDurationsMs  []int    `json:"charDurationsMs"`
+	Chars            []string `json:"chars"`
+}
---
+++ main.go
@@ -0,0 +1,490 @@
+// Client implementation for Eleven Labs TTS API:
+// 		- Streaming Websocket: wss://api.elevenlabs.io/v1/text-to-speech/:voice_id/stream-input
+// 		- Multi-Context Websocket: wss://api.elevenlabs.io/v1/text-to-speech/:voice_id/multi-stream-input
+
+package elevenlabs_websockets
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	neturl "net/url"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const JSON_CONTENT_TYPE = "application/json"
+const ELEVEN_BASEURL_HTTPS = "https://api.elevenlabs.io/v1"
+const ELEVEN_BASEURL_WSS = "wss://api.elevenlabs.io/v1"
+const MULTI_CONTEXT_MAX_REQUESTS = 5
+const STD_MAX_REQUESTS = 1
+
+type Client struct {
+	apiKey         string
+	timeout        time.Duration
+	ctx            context.Context
+	activeRequests map[string]struct{} // Active multi-context requests
+	cmu            sync.RWMutex
+}
+
+type VoiceSettings struct {
+	SimilarityBoost float32 `json:"similarity_boost"`
+	Stability       float32 `json:"stability"`
+	Style           float32 `json:"style,omitempty"`
+	SpeakerBoost    bool    `json:"use_speaker_boost,omitempty"`
+}
+
+type GenerationConfig struct {
+	ChunkLengthSchedule []int `json:"chunk_length_schedule"`
+}
+
+type QueryFunc func(*neturl.Values)
+
+type StreamingInputResponse struct {
+	Audio               string                    `json:"audio"`
+	IsFinal             bool                      `json:"isFinal"`
+	NormalizedAlignment StreamingAlignmentSegment `json:"normalizedAlignment"`
+	Alignment           StreamingAlignmentSegment `json:"alignment"`
+}
+
+type StreamingOutputResponse struct {
+	IsFinal             bool                      `json:"isFinal"`
+	NormalizedAlignment StreamingAlignmentSegment `json:"normalizedAlignment"`
+	Alignment           StreamingAlignmentSegment `json:"alignment"`
+}
+
+type StreamingOutputMultiCtxRawResponse struct {
+	IsFinal             bool                      `json:"isFinal"`
+	NormalizedAlignment StreamingAlignmentSegment `json:"normalizedAlignment"`
+	Alignment           StreamingAlignmentSegment `json:"alignment"`
+	ContextId           string                    `json:"contextId"`
+	ContextIdAlt        string                    `json:"context_id"`
+}
+
+type StreamingOutputMultiCtxResponse struct {
+	IsFinal             bool                      `json:"isFinal"`
+	NormalizedAlignment StreamingAlignmentSegment `json:"normalizedAlignment"`
+	Alignment           StreamingAlignmentSegment `json:"alignment"`
+	ContextId           string                    `json:"contextId"`
+}
+
+type textChunk struct {
+	Text                 string `json:"text"`
+	TryTriggerGeneration bool   `json:"try_trigger_generation"`
+}
+
+type TextToSpeechInputStreamingRequest struct {
+	Text                 string            `json:"text"`
+	TryTriggerGeneration bool              `json:"try_trigger_generation"`
+	VoiceSettings        *VoiceSettings    `json:"voice_settings,omitempty"`
+	GenerationConfig     *GenerationConfig `json:"generation_config,omitempty"`
+}
+
+type TextToSpeechInputMultiStreamingRequest struct {
+	ContextID            string            `json:"contextId"`
+	Text                 string            `json:"text"`
+	TryTriggerGeneration bool              `json:"try_trigger_generation"`
+	VoiceSettings        *VoiceSettings    `json:"voice_settings,omitempty"`
+	GenerationConfig     *GenerationConfig `json:"generation_config,omitempty"`
+}
+
+type WsStreamingOutputChannel chan StreamingOutputResponse
+
+// Standard Websocket Client
+func NewClient(ctx context.Context, apiKey string, reqTimeout time.Duration) *Client {
+	return &Client{apiKey: apiKey, timeout: reqTimeout, ctx: ctx}
+}
+
+// Multi-Context Websocket Client
+func NewMultiClient(ctx context.Context, apiKey string, reqTimeout time.Duration) *Client {
+	return &Client{apiKey: apiKey, timeout: reqTimeout, ctx: ctx, activeRequests: make(map[string]struct{})}
+}
+
+// API Get user
+func User(apiKey string) (*UserData, error) {
+	//uri := strings.Replace(c.API.URI, ":route", route, -1)
+	url := fmt.Sprintf("%s/user", ELEVEN_BASEURL_HTTPS)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating request: %w", err)
+	}
+	req.Header.Set("xi-api-key", apiKey)
+
+	client := &http.Client{
+		Timeout: 1 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var r UserData
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &r, nil
+}
+
+// Standard Websocket Request
+func (c *Client) StreamingRequest(TextReader chan string, AlignmentResponseChannel chan StreamingOutputResponse, AudioResponsePipe io.Writer, voiceID string, modelID string, req TextToSpeechInputStreamingRequest, queries ...QueryFunc) error {
+	driverActive := true // Driver shut down?
+	driverError := false // Unexpected errors
+
+	url := fmt.Sprintf("%s/text-to-speech/%s/stream-input?model_id=%s", ELEVEN_BASEURL_WSS, voiceID, modelID)
+
+	headers := http.Header{}
+	headers.Add("Accept", "*/*")
+	headers.Add("Content-Type", JSON_CONTENT_TYPE)
+	if c.apiKey != "" {
+		headers.Add("xi-api-key", c.apiKey)
+	}
+
+	u, err := neturl.Parse(url)
+	if err != nil {
+		return err
+	}
+
+	q := u.Query()
+	for _, qf := range queries {
+		qf(&q)
+	}
+	u.RawQuery = q.Encode()
+
+	conn, _, err := websocket.DefaultDialer.DialContext(c.ctx, u.String(), headers)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Send initial request
+	if err := conn.WriteJSON(req); err != nil {
+		return err
+	}
+
+	// Input watcher
+	inputCtx, inputCancel := context.WithCancel(context.Background())
+
+	errCh := make(chan error, 1)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Response watching
+	go func(wg *sync.WaitGroup, errCh chan<- error) {
+		defer wg.Done()
+		for {
+			select {
+			case <-c.ctx.Done():
+				return
+			default:
+				if !driverActive {
+					return
+				}
+				var input StreamingInputResponse
+				var response StreamingOutputResponse
+				if err := conn.ReadJSON(&input); err != nil {
+					if driverActive {
+						errCh <- err
+						driverError = true
+						inputCancel()
+					}
+					return
+				}
+
+				b, err := base64.StdEncoding.DecodeString(input.Audio)
+				if err != nil {
+					if driverActive {
+						errCh <- err
+						driverError = true
+						inputCancel()
+					}
+					return
+				}
+				// Send audio through the pipeline
+				if _, err := AudioResponsePipe.Write(b); err != nil {
+					break
+				}
+
+				// Send non-audio via the response channel
+				response = StreamingOutputResponse{
+					IsFinal:             input.IsFinal,
+					NormalizedAlignment: input.NormalizedAlignment,
+					Alignment:           input.Alignment,
+				}
+				AlignmentResponseChannel <- response
+			}
+		}
+	}(&wg, errCh)
+
+	// Input watching
+InputWatcher:
+	for {
+		select {
+		case <-inputCtx.Done():
+			driverActive = false
+			break InputWatcher
+		case <-c.ctx.Done():
+			driverActive = false
+			break InputWatcher
+		case chunk, ok := <-TextReader:
+			if !ok || !driverActive {
+				break InputWatcher
+			}
+			ch := &textChunk{Text: chunk, TryTriggerGeneration: true}
+			if err := conn.WriteJSON(ch); err != nil {
+				errCh <- err
+				break InputWatcher
+			}
+		}
+	}
+
+	// Send final "" to close out TTS buffer
+	if driverActive && !driverError {
+		if err := conn.WriteJSON(map[string]string{"text": ""}); err != nil {
+			if c.ctx.Err() == nil {
+				errCh <- err
+			}
+		}
+	}
+	conn.Close()
+	wg.Wait()
+
+	// Errors?
+	select {
+	case readErr := <-errCh:
+		if driverActive || driverError {
+			// Only send if the driver is active or the unexpected error flag is active
+			return readErr
+		} else {
+			return nil
+		}
+	default:
+	}
+
+	return nil
+}
+
+func (c *Client) activeContextCount() int {
+	c.cmu.Lock()
+	defer c.cmu.Unlock()
+	return len(c.activeRequests)
+}
+
+func (c *Client) hasMultiCtx(id string) bool {
+	c.cmu.RLock()
+	_, ok := c.activeRequests[id]
+	c.cmu.RUnlock()
+	return ok
+}
+
+func (c *Client) addMultiCtx(id string) {
+	c.cmu.Lock()
+	c.activeRequests[id] = struct{}{}
+	c.cmu.Unlock()
+}
+
+func (c *Client) removeMultiCtx(id string) {
+	c.cmu.Lock()
+	delete(c.activeRequests, id)
+	c.cmu.Unlock()
+}
+
+func (c *Client) HasCapacity() bool {
+	c.cmu.RLock()
+	defer c.cmu.RUnlock()
+	return len(c.activeRequests) < MULTI_CONTEXT_MAX_REQUESTS
+}
+
+func (c *Client) MultiCtxStreamingRequest(TextReader chan string, AlignmentResponseChannel chan StreamingOutputResponse, AudioResponsePipe io.Writer, voiceID string, modelID string, req TextToSpeechInputMultiStreamingRequest, queries ...QueryFunc) error {
+	driverActive := true // Driver shut down?
+	driverError := false // Unexpected errors
+
+	// Eval context and capacity
+	if !c.hasMultiCtx(req.ContextID) {
+		if c.activeContextCount() >= MULTI_CONTEXT_MAX_REQUESTS {
+			return fmt.Errorf("active requests reached: %d", MULTI_CONTEXT_MAX_REQUESTS)
+		}
+		c.addMultiCtx(req.ContextID)
+	}
+
+	// Make request
+	url := fmt.Sprintf("%s/text-to-speech/%s/multi-stream-input?model_id=%s", ELEVEN_BASEURL_WSS, voiceID, modelID)
+	headers := http.Header{}
+	headers.Add("Accept", "*/*")
+	headers.Add("Content-Type", JSON_CONTENT_TYPE)
+	if c.apiKey != "" {
+		headers.Add("xi-api-key", c.apiKey)
+	}
+
+	u, err := neturl.Parse(url)
+	if err != nil {
+		return err
+	}
+
+	q := u.Query()
+	for _, qf := range queries {
+		qf(&q)
+	}
+	u.RawQuery = q.Encode()
+
+	conn, _, err := websocket.DefaultDialer.DialContext(c.ctx, u.String(), headers)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Send initial request
+	if err := conn.WriteJSON(req); err != nil {
+		return err
+	}
+
+	// Input watcher
+	inputCtx, inputCancel := context.WithCancel(context.Background())
+
+	errCh := make(chan error, 1)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Response watching
+	go func(wg *sync.WaitGroup, errCh chan<- error) {
+		defer wg.Done()
+		for {
+			select {
+			case <-c.ctx.Done():
+				return
+			default:
+				if !driverActive {
+					return
+				}
+				var input StreamingInputResponse
+				var response StreamingOutputResponse
+				if err := conn.ReadJSON(&input); err != nil {
+					if driverActive {
+						errCh <- err
+						driverError = true
+						inputCancel()
+					}
+					return
+				}
+
+				b, err := base64.StdEncoding.DecodeString(input.Audio)
+				if err != nil {
+					if driverActive {
+						errCh <- err
+						driverError = true
+						inputCancel()
+					}
+					return
+				}
+				// Send audio through the pipeline
+				if _, err := AudioResponsePipe.Write(b); err != nil {
+					break
+				}
+
+				// Send non-audio via the response channel
+				response = StreamingOutputResponse{
+					IsFinal:             input.IsFinal,
+					NormalizedAlignment: input.NormalizedAlignment,
+					Alignment:           input.Alignment,
+				}
+				AlignmentResponseChannel <- response
+			}
+		}
+	}(&wg, errCh)
+
+	// Input watching
+InputWatcher:
+	for {
+		select {
+		case <-inputCtx.Done():
+			driverActive = false
+			break InputWatcher
+		case <-c.ctx.Done():
+			driverActive = false
+			break InputWatcher
+		case chunk, ok := <-TextReader:
+			if !ok || !driverActive {
+				break InputWatcher
+			}
+			ch := &textChunk{Text: chunk, TryTriggerGeneration: true}
+			if err := conn.WriteJSON(ch); err != nil {
+				errCh <- err
+				break InputWatcher
+			}
+		}
+	}
+
+	// Send final "" to close out TTS buffer
+	if driverActive && !driverError {
+		if err := conn.WriteJSON(map[string]string{"text": ""}); err != nil {
+			if c.ctx.Err() == nil {
+				errCh <- err
+			}
+		}
+	}
+	conn.Close()
+	wg.Wait()
+
+	// Errors?
+	select {
+	case readErr := <-errCh:
+		c.removeMultiCtx(req.ContextID)
+		if driverActive || driverError {
+			// Only send if the driver is active or the unexpected error flag is active
+			return readErr
+		} else {
+			return nil
+		}
+	default:
+	}
+	c.removeMultiCtx(req.ContextID)
+
+	return nil
+}
+
+func LanguageCode(value string) QueryFunc {
+	return func(q *neturl.Values) {
+		q.Add("language_code", value)
+	}
+}
+
+func OutputFormat(value string) QueryFunc {
+	return func(q *neturl.Values) {
+		q.Add("output_format", value)
+	}
+}
+
+func SyncAlignment(value string) QueryFunc {
+	return func(q *neturl.Values) {
+		q.Add("sync_alignment", value)
+	}
+}
+
+func InactivityTimeout(value string) QueryFunc {
+	return func(q *neturl.Values) {
+		q.Add("inactivity_timeout", value)
+	}
+}
+
+func EnableSsmlParsing(value string) QueryFunc {
+	return func(q *neturl.Values) {
+		q.Add("enable_ssml_parsing", value)
+	}
+}
